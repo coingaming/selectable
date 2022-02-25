93abf107967fca810a1207c823e87c84736015e0
Reio Piller <reio@heathmont.net>
2022-02-25 14:46:03 +0200

    fix: semantic-release

--- mix.exs
+++ mix.exs
@@ -2,13 +2,20 @@ defmodule Selectable.MixProject do
   use Mix.Project
 
   def project do
+    version = version()
+
     [
       app: :selectable,
-      version: "0.1.0",
+      version: version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
-      deps: deps()
+      deps: deps(),
+      package: package(version),
+      # docs
+      name: "Selectable",
+      source_url: "https://github.com/coingaming/selectable/tree/v#{version}",
+      homepage_url: "https://github.com/coingaming/selectable/tree/v#{version}"
     ]
   end
 
@@ -19,11 +26,28 @@ defmodule Selectable.MixProject do
     ]
   end
 
+  defp version do
+    case File.read("VERSION") do
+      {:ok, version} -> String.trim(version)
+      {:error, _} -> "0.0.0-development"
+    end
+  end
+
+  defp package(version) do
+    [
+      organization: "coingaming",
+      licenses: ["UNLICENSED"],
+      files: ["lib", "mix.exs", "README.md", "VERSION"],
+      links: %{
+        "GitHub" => "https://github.com/coingaming/selectable/tree/v#{version}"
+      }
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false},
     ]
   end
 end
