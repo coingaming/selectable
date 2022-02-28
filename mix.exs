10d10f2d35bbb7ad337c018805cb459f86e787ef
Reio Piller <reio@heathmont.net>
2022-02-28 13:27:21 +0200

    fix: read VERSION in compile time

--- mix.exs
+++ mix.exs
@@ -1,21 +1,27 @@
 defmodule Selectable.MixProject do
   use Mix.Project
+  
+  @version (case File.read("VERSION") do
+    {:ok, version} -> String.trim(version)
+    {:error, _} -> "0.0.0-development"
+  end)
 
   def project do
-    version = version()
-
     [
       app: :selectable,
-      version: version,
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
       deps: deps(),
-      package: package(version),
+      package: package(),
       # docs
       name: "Selectable",
-      source_url: "https://github.com/coingaming/selectable/tree/v#{version}",
-      homepage_url: "https://github.com/coingaming/selectable/tree/v#{version}"
+      source_url: "https://github.com/coingaming/selectable",
+      homepage_url: "https://github.com/coingaming/selectable/tree/v#{@version}",
+      docs: [
+        source_ref: "v#{@version}"
+      ]
     ]
   end
 
@@ -26,20 +32,13 @@ defmodule Selectable.MixProject do
     ]
   end
 
-  defp version do
-    case File.read("VERSION") do
-      {:ok, version} -> String.trim(version)
-      {:error, _} -> "0.0.0-development"
-    end
-  end
-
-  defp package(version) do
+  defp package do
     [
       organization: "coingaming",
       licenses: ["UNLICENSED"],
       files: ["lib", "mix.exs", "README.md", "VERSION"],
       links: %{
-        "GitHub" => "https://github.com/coingaming/selectable/tree/v#{version}"
+        "GitHub" => "https://github.com/coingaming/selectable/tree/v#{@version}"
       }
     ]
   end
